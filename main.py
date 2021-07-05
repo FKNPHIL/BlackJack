89b8459aae079cf3f82c36f194a4f4bc56cee9c2
FKN PHIL <75670155+FKNPHIL@users.noreply.github.com>
2021-07-05 18:30:33 -0230

    made a deck of cards

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
+def makeCards():
+    suits = ["clubs", "hearts", "spades", "diamonds"]
+    ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+    values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11]
+
+    deck = []
+    for suit in suits:
+        counter = 0
+        for rank in ranks:
+            newCard = []
+            newCard.append(suit)
+            newCard.append(rank)
+            newCard.append(values[counter])
+            deck.append(newCard)
+            counter += 1
+    return deck
+
 def main():
-    pass
+    print(makeCards())
 
 if __name__ == '__main__':
     main()

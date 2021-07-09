e4e0b2d45a538b24ba49963242d4af618a792929
FKN PHIL <75670155+FKNPHIL@users.noreply.github.com>
2021-07-08 23:43:16 -0230

    correct wager inputs for max wager

--- BlackJack.py
+++ BlackJack.py
@@ -4,7 +4,7 @@ import random
 
 
 def makeCards():
-    suits = ["clubs", "hearts", "spades", "diamonds"]
+    suits = ["♣", "♥", "♠", "♦"]
     ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
     values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11]
 
@@ -21,20 +21,21 @@ def makeCards():
     return deck
 
 def main():
+    deck = makeCards()
     print("WELCOME TO BLACKJACK!")
     print("BlackJack Payout is 3:2\n")
-    print("Your Available Money: "+ str(db.viewMoney()))
-
+    print("Your Available Money: " + str(db.viewMoney()))
+    if db.viewMoney() <= 4:
+        getMoney = pG.getMoney()
     while True:
         choice = pG.getChoice()
-        deck = makeCards()
         wager = pG.getWager()
         playerCards = pG.playerCards(deck)
         dealerCards = pG.dealerCards(deck)
         playerScore = pG.getScores(playerCards)
         dealerScore = pG.getScores(dealerCards)
-        print("\nDEALER'S SHOW CARD:")
-        print(dealerCards[0][1] +" "+ dealerCards[0][0])
+        print("\nDEALERS CARDS:")
+        print(dealerCards[0][1], dealerCards[0][0])
         print("? ???")
         print("\nYOUR CARDS:")
         for card in playerCards:
@@ -42,17 +43,17 @@ def main():
         print()
 
         if len(playerCards) == 2:
-            if(playerCards[0][2]) == 11 and (playerCards[1][2]) == 11:
+            if (playerCards[0][2]) == 11 and (playerCards[1][2]) == 11:
                 playerCards[0][2] = 1
                 playerScore -= 10
 
         if len(dealerCards) == 2:
-            if(dealerCards[0][2]) == 11 and (dealerCards[1][2]) == 11:
+            if (dealerCards[0][2]) == 11 and (dealerCards[1][2]) == 11:
                 dealerCards[0][2] = 1
                 dealerScore -= 10
 
         while playerScore <= 21:
-            playerChoice = input("h to Hit or s to Stand (h/s): ")
+            playerChoice = input("(H)it or (S)tand >>")
             if playerChoice.lower() != "h" and playerChoice.lower() != "s":
                 print("You must enter either 'h' or 's'")
                 continue
@@ -71,11 +72,9 @@ def main():
                 break
             if playerScore > 21:
                 print("YOU BUSTED")
-                continue
+                break
 
-
-
-        while dealerScore <=17:
+        while dealerScore <=17 and playerScore <= 21:
             print("DEALER MUST HIT")
             dealerCard = random.choice(deck)
             dealerCards.append(dealerCard)
@@ -85,7 +84,7 @@ def main():
             dealerScore = pG.getScores(dealerCards)
 
         if dealerScore == 21:
-            print("BLACKJACK")
+            print("DEALER HAS BLACKJACK")
         if dealerScore > 21:
             print("DEALER BUSTED")
         if playerScore == 21 and dealerScore > 17:
@@ -96,6 +95,19 @@ def main():
         print("Players Score is "+str(playerScore))
         print("DealerScore is "+str(dealerScore))
         didWin = pG.checkForWin(playerScore, dealerScore)
+        if didWin == 1:
+            bjWin = db.winMoney(wager * 1.5)
+        if didWin == 0:
+            bjWin = db.winMoney(wager)
+        print("Your Available Money: "+ str(db.viewMoney()))
+        if (db.viewMoney()) <= 4:
+            print("\nYOU HAVE BEEN BANKRUPTED! GOOD THING YOU WEREN'T PLAYING FOR REAL MONEY!")
+            getmoney = pG.getMoney()
+
+        if len(deck) <=26:
+            print("\nHalf deck used, dealer is reshuffling...")
+            deck = makeCards()
+            input("Deck shuffled press enter to continue...")
 
 
 if __name__ == '__main__':
--- playGame.py
+++ playGame.py
@@ -1,24 +1,30 @@
 import random
+import db as db
+import sys
 
 def getChoice():
-    while True:
-        choice = input("Would you like to play a game (y/n) >>")
-        if choice.lower() == "y":
-            return choice
-        if choice.lower() == "n":
-            print("OK GOODBYE!")
-            exit()
-        if choice.lower() != "y":
-            print("YOU MUST ENTER 'Y' or 'N'")
-            continue
+   choice = input("\nWould you like to play a game? (Y)es or (N)o >>")
+   if choice.lower() == "y":
+       return choice
+   if choice.lower() == "n":
+       print("OK GOODBYE!")
+       exit()
+   if choice.lower() != "y":
+       print("YOU MUST ENTER 'Y' or 'N'")
+
 
 def getWager():
+    maxWagerAmount = db.viewMoney()
     while True:
         try:
             wagerAmount = int(input("Enter your wager amount (5 to 1000) >>"))
             if wagerAmount < 5 or wagerAmount > 1000:
                 print("You must enter a correct number between 5 and 1000")
                 continue
+            if wagerAmount > maxWagerAmount:
+                print("You only have " + str(maxWagerAmount) + " available to wager")
+                continue
+            bjLoss = db.betMoney(wagerAmount)
             return wagerAmount
         except ValueError:
             print("You must enter a number between 5 and 1000")
@@ -66,7 +72,7 @@ def getScores(handCards):
 def playerChoice(deck):
     while True:
         try:
-            playerChoice = input("h to Hit or s to Stand (h/s): ")
+            playerChoice = input("(H)it or (S)tand >>")
             if playerChoice.lower() != "h" and playerChoice.lower() != "s":
                 print("You must enter either 'h' or 's'")
                 continue
@@ -102,3 +108,18 @@ def checkForWin(playerScores, dealerScores):
     if (playerScores < dealerScores):
         print("YOU HAVE LOST SORRY!")
         return -1
+
+def getMoney():
+    getMoney = input("You don't have enough money. Would you like to get more? (Y)es or (N)o >>")
+    while True:
+        if getMoney.lower() == "y":
+            bjWin = db.winMoney(1000)
+            input("1000 added. Press enter to continue....")
+            break
+        if getMoney.lower() == "n":
+            print("You need money to wager. Exiting program")
+            sys.exit()
+        else:
+            print("You need to enter 'y' or 'n'")
+            print("Error occurred Exiting program")
+            sys.exit()
